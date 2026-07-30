1c484354128db6ed9cd4b3e2cab515b48b4ed24f
Piotr Majkrzak <piotr@majkrzak.dev>
2026-07-30 20:15:14 +0200

    Initialize empty registry

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    import-tree.url = "github:vic/import-tree";
+  };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./flake);
+}
