e5446ea141a4ac217f456627cf0cc287fc2f06b7
Piotr Majkrzak <piotr@majkrzak.dev>
2026-08-07 18:57:05 +0200

    Add bucket support for ACME certificates

--- nixos-modules/buckets.nix
+++ nixos-modules/buckets.nix
@@ -7,10 +7,11 @@
 let
   inherit (lib)
     mkOption
-    mapAttrs'
+    attrValues
     listToAttrs
-    filterAttrs
     mkIf
+    filter
+    unique
     nameValuePair
     ;
   inherit (lib.types)
@@ -21,7 +22,36 @@ let
     submodule
     ;
   sopsCfg = config.sops;
-  cfg = filterAttrs (_name: fs: (fs.bucket.name != null)) config.fileSystems;
+
+  buckets = unique (
+    filter (bucket: bucket.name != null) (
+      (map (fs: fs.bucket) (attrValues config.fileSystems))
+      ++ (map (cert: cert.bucket) (attrValues config.security.acme.certs))
+    )
+  );
+  bucketOps = {
+    name = mkOption {
+      type = nullOr str;
+      default = null;
+      description = "Name of the S3 bucket.";
+    };
+    endpoint = mkOption {
+      type = str;
+      description = "Endpoint of the S3 service.";
+    };
+    access_key_id = mkOption {
+      type = str;
+      description = "Access key identifier.";
+    };
+    secret_access_key = mkOption {
+      type = path;
+      description = "Access key secret SOPS binnary file.";
+    };
+    region = mkOption {
+      type = str;
+      description = "S3 region.";
+    };
+  };
 in
 {
   options.fileSystems = mkOption {
@@ -32,29 +62,7 @@ in
           cfg' = config.bucket;
         in
         {
-          options.bucket = {
-            name = mkOption {
-              type = nullOr str;
-              default = null;
-              description = "Name of the S3 bucket.";
-            };
-            endpoint = mkOption {
-              type = str;
-              description = "Endpoint of the S3 service.";
-            };
-            access_key_id = mkOption {
-              type = str;
-              description = "Access key identifier.";
-            };
-            secret_access_key = mkOption {
-              type = path;
-              description = "Access key secret SOPS binnary file.";
-            };
-            region = mkOption {
-              type = str;
-              description = "S3 region.";
-            };
-          };
+          options.bucket = bucketOps;
           config = mkIf (cfg'.name != null) {
             device = "s3:${cfg'.name}";
             fsType = "rclone";
@@ -66,30 +74,54 @@ in
       )
     ]);
   };
+  options.security.acme.certs = mkOption {
+    type = attrsOf (submodule [
+      (
+        { config, name, ... }:
+        let
+          cfg' = config.bucket;
+        in
+        {
+          options.bucket = bucketOps;
+          config = mkIf (cfg'.name != null) {
+            postRun = ''
+              ${pkgs.rclone}/bin/rclone \
+                copy . s3:certs/${name} \
+                --config=${sopsCfg.templates."${cfg'.name}-rclone.conf".path}
+            '';
+          };
+        }
+      )
+    ]);
+  };
   config = {
     environment.systemPackages = with pkgs; [ rclone ];
-    sops.secrets = mapAttrs' (
-      _: fs:
-      nameValuePair fs.bucket.access_key_id {
-        sopsFile = fs.bucket.secret_access_key;
-        format = "binary";
-        mode = "0400";
-      }
-    ) cfg;
-    sops.templates = mapAttrs' (
-      _: fs:
-      nameValuePair "${fs.bucket.name}-rclone.conf" {
-        content = ''
-          [s3]
-          type = s3
-          provider = Other
-          endpoint = ${fs.bucket.endpoint}
-          access_key_id = ${fs.bucket.access_key_id}
-          secret_access_key = ${config.sops.placeholder.${fs.bucket.access_key_id}}
-          region = ${fs.bucket.region}
-          env_auth = false
-        '';
-      }
-    ) cfg;
+    sops.secrets = listToAttrs (
+      map (
+        bucket:
+        nameValuePair bucket.access_key_id {
+          sopsFile = bucket.secret_access_key;
+          format = "binary";
+          mode = "0400";
+        }
+      ) buckets
+    );
+    sops.templates = listToAttrs (
+      map (
+        bucket:
+        nameValuePair "${bucket.name}-rclone.conf" {
+          content = ''
+            [s3]
+            type = s3
+            provider = Other
+            endpoint = ${bucket.endpoint}
+            access_key_id = ${bucket.access_key_id}
+            secret_access_key = ${config.sops.placeholder.${bucket.access_key_id}}
+            region = ${bucket.region}
+            env_auth = false
+          '';
+        }
+      ) buckets
+    );
   };
 }
